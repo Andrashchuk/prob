ced07544129b65596540d60c1b8ae61391dca074
Viktor Andrashchuk <72516654+Andrashchuk@users.noreply.github.com>
2022-06-14 00:49:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Bank
+{
+
+    public class BankAccount
+    {
+      
+
+        public int Deposit(int deposit, double percent)
+        {
+            return (int)(deposit * percent);
+        }
+        static void Main()
+        {
+
+        }
+    }
+
+}
+
